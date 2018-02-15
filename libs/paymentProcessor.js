9227b2ee3a55079959d17714a909ef4007ed0bcb
CryptoForge <36178381+CryptoForge@users.noreply.github.com>
2018-02-14 19:46:36 -0800

    Update paymentProcessor.js

--- libs/paymentProcessor.js
+++ libs/paymentProcessor.js
@@ -64,7 +64,13 @@ function SetupForPool(poolOptions, setupFinished) {
 
     var processingConfig = poolOptions.paymentProcessing;
 
-
+    // pplnt - pay per last N time shares
+    var pplntEnabled = processingConfig.paymentMode === "pplnt" || false;
+    var pplntTimeQualify = processingConfig.pplnt || 0.51; // 51%
+    logger.debug(logSystem, logComponent, logComponent + ' PPLNT: ' + pplntEnabled + ', time period: '+pplntTimeQualify);
+    
+    var getMarketStats = poolOptions.coin.getMarketStats === true;
+    
     // TODO fix logger, broken intentionally, wil fix after final migration to winston
     var daemon = new Stratum.daemon.interface([processingConfig.daemon], undefined);
 
@@ -141,7 +147,118 @@ function SetupForPool(poolOptions, setupFinished) {
         setupFinished(true);
     });
 
+    function cacheMarketStats() {
+        
+        var marketStatsUpdate = [];
+        var coin = logComponent.replace('_testnet', '').toLowerCase();
 
+        if (coin == 'zen')
+            coin = 'zencash';
+
+        request('https://api.coinmarketcap.com/v1/ticker/'+coin+'/', function (error, response, body) {
+            if (error) {
+                logger.error(logSystem, logComponent, 'Error with http request to https://api.coinmarketcap.com/ ' + JSON.stringify(error));
+                return;
+            }
+            if (response && response.statusCode) {
+                if (response.statusCode == 200) {
+                    if (body) {
+                        var data = JSON.parse(body);
+                        if (data.length > 0) {
+                            marketStatsUpdate.push(['hset', logComponent + ':stats', 'coinmarketcap', JSON.stringify(data)]);
+                            redisClient.multi(marketStatsUpdate).exec(function(err, results){
+                                if (err){
+                                    logger.error(logSystem, logComponent, 'Error with redis during call to cacheMarketStats() ' + JSON.stringify(error));
+                                    return;
+                                }
+                            });
+                        }
+                    }
+                } else {
+                    logger.error(logSystem, logComponent, 'Error, unexpected http status code during call to cacheMarketStats() ' + JSON.stringify(response.statusCode));
+                }
+            }
+        });
+    }
+
+    function cacheNetworkStats () {
+
+        var params = null;
+
+        daemon.cmd('getmininginfo', params,
+            function (result) {                
+                if (!result || result.error || result[0].error || !result[0].response) {
+                    logger.error(logSystem, logComponent, 'Error with RPC call getmininginfo '+JSON.stringify(result[0].error));
+                    return;
+                }
+
+                var coin = logComponent;
+                var finalRedisCommands = [];
+
+                if (result[0].response.blocks !== null) {
+                    finalRedisCommands.push(['hset', coin + ':stats', 'networkBlocks', result[0].response.blocks]);
+                }
+
+                if (result[0].response.difficulty !== null) {
+                    finalRedisCommands.push(['hset', coin + ':stats', 'networkDiff', result[0].response.difficulty]);
+                }
+                if (result[0].response.networkhashps !== null) {
+                    finalRedisCommands.push(['hset', coin + ':stats', 'networkSols', result[0].response.networkhashps]);
+                }
+
+                daemon.cmd('getnetworkinfo', params,
+                    function (result) {
+                        if (!result || result.error || result[0].error || !result[0].response) {
+                            logger.error(logSystem, logComponent, 'Error with RPC call getnetworkinfo '+JSON.stringify(result[0].error));
+                            return;
+                        }
+                    
+                        if (result[0].response.connections !== null) {
+                            finalRedisCommands.push(['hset', coin + ':stats', 'networkConnections', result[0].response.connections]);
+                        }
+                        if (result[0].response.version !== null) {
+                            finalRedisCommands.push(['hset', coin + ':stats', 'networkVersion', result[0].response.version]);
+                        }
+
+                        if (result[0].response.subversion !== null) {
+                            finalRedisCommands.push(['hset', coin + ':stats', 'networkSubVersion', result[0].response.subversion]);
+                        }
+                        if (result[0].response.protocolversion !== null) {
+                            finalRedisCommands.push(['hset', coin + ':stats', 'networkProtocolVersion', result[0].response.protocolversion]);
+                        }
+
+                        if (finalRedisCommands.length <= 0)
+                            return;
+
+                        redisClient.multi(finalRedisCommands).exec(function(error, results){
+                            if (error){
+                                logger.error(logSystem, logComponent, 'Error with redis during call to cacheNetworkStats() ' + JSON.stringify(error));
+                                return;
+                            }
+                        });
+                    }
+                );
+            }
+        );
+    }
+    
+    // network stats caching every 58 seconds
+    var stats_interval = 58 * 1000;
+    var statsInterval = setInterval(function() {
+        // update network stats using coin daemon
+        cacheNetworkStats();
+    }, stats_interval);
+
+    // market stats caching every 5 minutes
+    if (getMarketStats === true) {
+        var market_stats_interval = 300 * 1000;
+        var marketStatsInterval = setInterval(function() {
+            // update market stats using coinmarketcap
+            cacheMarketStats();
+        }, market_stats_interval);
+    }
+    
+    
     /* Deal with numbers in smallest possible units (satoshis) as much as possible. This greatly helps with accuracy
        when rounding and whatnot. When we are storing numbers for only humans to see, store in whole coin units. */
 
